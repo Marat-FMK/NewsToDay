0e6a4c9d05669a08935e5a8bc766366efb55535d
Evgeniy <rabota24rus@mail.ru>
2024-10-20 19:11:24 +0300

    first commit

---
+++ NewsToDayApp/NewsToDayAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsToDayAppApp.swift
+//  NewsToDayApp
+//
+//  Created by Evgeniy on 20.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct NewsToDayAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
