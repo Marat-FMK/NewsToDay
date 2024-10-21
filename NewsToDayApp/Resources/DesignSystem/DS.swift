079c026cf3371f2fed4a02df187ce71c8ea89a86
DmKeller <dmit.keller@gmail.com>
2024-10-21 16:04:44 +0300

    fix colors

--- NewsToDayApp/Resources/DesignSystem/DS.swift
+++ NewsToDayApp/Resources/DesignSystem/DS.swift
@@ -9,12 +9,23 @@ import SwiftUI
 // MARK: - DS (Design System)
 enum DS {
     enum Colors {
-        static let whiteAccent = Color(hex: "#FFFFFF")
-        static let indigoAccent = Color(hex: "#475AD7")
-        static let primaryText = Color(hex: "#333647")
-        static let secondaryText = Color(hex: "#7C82A1")
+        static let purpleDark = Color(hex: "#2536A7")
+        static let purplePrimary = Color(hex: "#475AD7")
+        static let purpleLight = Color(hex: "#8A96E5")
+        static let purpltLighter = Color(hex: "#EEF0FB")
+        
+        static let grayDark = Color(hex: "#666C8E")
+        static let pgrayPrimary = Color(hex: "#7C82A1")
         static let grayLight = Color(hex: "#ACAFC3")
-        static let buttonText = Color(hex: "#666C8E")
-        static let buttonBackground = Color(hex: "#13353F2B")
+        static let grayLighter = Color(hex: "#F3F4F6")
+        
+        static let blackDark = Color(hex: "#22242F")
+        static let blackyPrimary = Color(hex: "#22242F")
+        static let blackLight = Color(hex: "#44485F")
+        static let blackLighter = Color(hex: "#555A77")
+    }
+    
+    enum Fonts {
+        
     }
 }
