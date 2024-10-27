0bde5fbee98a9c409189412f2594bc7c8166632f
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2024-10-27 18:30:18 +0300

    add View for recommended News

---
+++ NewsToDayApp/Screens/MainScreen/SubViws/AllRecomendedNewsView.swift
@@ -0,0 +1,61 @@
+//
+//  AllRecomendedNewsView.swift
+//  NewsToDayApp
+//
+//  Created by Marat Fakhrizhanov on 27.10.2024.
+//
+
+import SwiftUI
+
+struct AllRecomendedNewsView: View {
+    @Environment(\.dismiss) var dismiss
+    let news: [ArticleDTO]
+    
+    var body: some View {
+        
+        VStack(alignment: .leading) {
+            CustomToolBar(title: Resources.Text.allRecommendedNews, subTitle: Resources.Text.weHaveSelectedTheBestNewsForYou)
+                    .padding(.bottom,30)
+                    .padding(.top,30)
+            
+            ScrollView(showsIndicators: false) {
+            if news.isEmpty {
+                ForEach(1..<9) { _ in
+                    VStack {
+                        HStack {
+                            ShimmerView(cef: 2.56)
+                            ShimmerTextView()
+                        }
+                    }
+                }
+            } else {
+                ForEach(news) { news in
+                    NavigationLink{
+                        DetailView(title: news.title, link: news.link, creator: news.creator, description: news.description, category: news.category, isFavorite: news.isFavorite, imageUrl: news.imageUrl, action: {})
+                    } label: {
+                        RecommendedNewsView(title: news.title, imageUrl: news.imageUrl, category: news.category)
+                    }
+                }
+            }
+        }
+                .padding()
+    }
+        .ignoresSafeArea()
+        .navigationBarBackButtonHidden()
+        .toolbar {
+            ToolbarItem( placement: .topBarLeading) {
+                Button{
+                    dismiss()
+                } label: {
+                    Image(systemName: Resources.Image.arrowLeft)
+                        .frame(width: 24, height: 24)
+                        .foregroundStyle(.black)
+                }
+            }
+        }
+    }
+}
+
+//#Preview {
+//    AllRecomendedNewsView()
+//}
