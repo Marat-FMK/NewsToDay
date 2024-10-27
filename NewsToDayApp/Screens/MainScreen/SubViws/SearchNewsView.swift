90ee268628ffc2dc95c37e177e6a8d33773dfe3d
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2024-10-28 00:53:21 +0300

    project

---
+++ NewsToDayApp/Screens/MainScreen/SubViws/SearchNewsView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchNewsView.swift
+//  NewsToDayApp
+//
+//  Created by Marat Fakhrizhanov on 27.10.2024.
+//
+
+import SwiftUI
+
+struct SearchNewsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchNewsView()
+}
