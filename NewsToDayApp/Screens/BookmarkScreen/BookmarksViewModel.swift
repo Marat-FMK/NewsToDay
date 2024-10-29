9a7671be00fd5571e517edb591660c48a823b305
DmKeller <dmit.keller@gmail.com>
2024-10-29 20:57:53 +0300

    fix bookmarks

--- NewsToDayApp/Screens/BookmarkScreen/BookmarksViewModel.swift
+++ NewsToDayApp/Screens/BookmarkScreen/BookmarksViewModel.swift
@@ -7,23 +7,36 @@
 
 import Foundation
 
+/// ViewModel for managing bookmarked news articles.
 @MainActor
 final class BookmarksViewModel: ObservableObject {
+    /// Published property to store a set of bookmarks.
     @Published var bookmarks: Set<ArticleDTO> = []
   
     private var bookmarkManager: IBookMarks
     
+    // MARK: - Initializer
+    
+    /// Initializer that allows dependency injection of `IBookMarks`.
+    /// - Parameter bookmarkManager: Bookmark manager, default is `BookMarksManager.shared`.
     init(bookmarkManager: IBookMarks = BookMarksManager.shared) {
         self.bookmarkManager = bookmarkManager
     }
     
+    // MARK: - Bookmark Fetching
+    
+    /// Fetches bookmarks from the bookmark manager and updates the `bookmarks` set.
     func fetchBookmarks() {
         Task {
             let bookmarkEntities = bookmarkManager.fetchBookmarks()
-            self.bookmarks = Set(bookmarkEntities.map { ArticleDTO(from: $0)})
+            self.bookmarks = Set(bookmarkEntities.map { ArticleDTO(from: $0) })
         }
     }
     
+    // MARK: - Bookmark Deletion
+    
+    /// Deletes a specific bookmark by its `id`.
+    /// - Parameter id: The identifier of the article to delete from bookmarks.
     func deleteBookmark(withId id: String) {
         if let article = bookmarks.first(where: { $0.id == id }) {
             bookmarks.remove(article)
@@ -31,7 +44,9 @@ final class BookmarksViewModel: ObservableObject {
         }
     }
     
+    /// Deletes all bookmarks from the storage.
     func deleteAllBookmarks() {
         bookmarkManager.deleteAllBookmarks()
+        bookmarks.removeAll()
     }
 }
